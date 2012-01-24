bc796ff85fae5f7eb42b645771a4435e257e4713
Djalma Araujo <djalma.araujo@gmail.com>
2012-01-24 15:51:57 -0300

    Initial Commit

---
+++ Biblia IBSM/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Biblia IBSM
+//
+//  Created by DJALMA ANDRADE on 1/24/12.
+//  Copyright djalmaaraujo 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
